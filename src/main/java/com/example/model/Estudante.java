044a682c7e47b630e3b77962dd464988dc70edcb
Bruno Souza <souza.brunha@gmail.com>
2025-09-07 11:40:28 -0300

    first commit

---
+++ src/main/java/com/example/model/Estudante.java
@@ -0,0 +1,47 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Estudante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int matricula;
+    private String nome;
+    private String curso;
+    private int anoEntrada;
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getAnoEntrada() {
+        return anoEntrada;
+    }
+
+    public void setAnoEntrada(int anoEntrada) {
+        this.anoEntrada = anoEntrada;
+    }
+}
---
+++ src/main/java/com/example/model/Professor.java
@@ -0,0 +1,48 @@
+package com.example.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Professor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int codigo;
+    private String nome;
+    private String curso;
+    private String materiaLecionada;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getMateriaLecionada() {
+        return materiaLecionada;
+    }
+
+    public void setMateriaLecionada(String materiaLecionada) {
+        this.materiaLecionada = materiaLecionada;
+    }    
+}
